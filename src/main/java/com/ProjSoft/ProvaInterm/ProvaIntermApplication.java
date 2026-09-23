7a9a684dee483229ab9baa58feebc69c523ff32e
Enzo Santoro <ez.santoro@gmail.com>
2026-09-23 07:45:38 -0300

    Initial commit

---
+++ src/main/java/com/ProjSoft/ProvaInterm/ProvaIntermApplication.java
@@ -0,0 +1,13 @@
+package com.ProjSoft.ProvaInterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaIntermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaIntermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjSoft/ProvaInterm/ProvaIntermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjSoft.ProvaInterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaIntermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
